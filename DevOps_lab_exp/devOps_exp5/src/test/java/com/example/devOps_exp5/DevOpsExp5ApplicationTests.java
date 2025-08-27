ef43365cb32d54bd8a67b42f3776e64067a4e7d1
aaryaP777 <patilaarya106@gmail.com>
2025-08-27 15:11:59 +0530

    exp 5 added

---
+++ DevOps_lab_exp/devOps_exp5/src/test/java/com/example/devOps_exp5/DevOpsExp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devOps_exp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsExp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
