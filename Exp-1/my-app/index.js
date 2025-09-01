be463cb9fe3d8233e50af30c3f7d2811b77e5ab0
Dhairya-A-Mehra <mpdhairyamehra000942@gmail.com>
2025-09-01 19:07:46 +0530

    Jenkins Logs and package.json updated for my-app

---
+++ Exp-1/my-app/index.js
@@ -0,0 +1,5 @@
+// Exp-1/my-app/index.js
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello, Jenkins CI/CD!'));
+app.listen(3000, () => console.log('Server running on port 3000'));
