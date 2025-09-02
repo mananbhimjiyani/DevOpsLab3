1edc251c186742eaa5b66d6dc6673f72449145d8
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2025-09-02 10:10:04 +0530

    Manan's Commit

---
+++ Exp5/retail-app/src/main/java/com/example/retail_app/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.retailapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
---
+++ Exp5/retail-app/src/main/java/com/example/retail_app/RetailAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.retail_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppApplication.class, args);
+	}
+
+}
